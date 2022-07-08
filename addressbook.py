c4efe0b5fa7105368cbf0ff4c2f082d818cf7405
Jay Miller <kjaymiller@gmail.com>
2022-07-08 08:59:04 -0700

    add team and twitter

--- addressbook.py
+++ addressbook.py
@@ -1,6 +1,5 @@
 """This demo is an address book application"""
 import dataclasses
-from distutils import extension
 
 
 @dataclasses.dataclass
@@ -13,7 +12,7 @@ class Address:
     
     def __str__(self):
         """Return a multi-line string representation of the address"""
-        "\n".join((self.street_address, self.city, self.region, self.country))
+        return "\n".join((self.street_address, self.city, self.region, self.country))
 
 @dataclasses.dataclass
 class BaseContact:
@@ -23,11 +22,17 @@ class BaseContact:
     address: Address = None
     notes: str = None
     website: str = None
+    twitter: str = None # The twitter username of the contact (without the @)
+
+    def get_twitter_url(self):
+        """Returns the full twitter url for the contact"""
+        return "https://twitter.com/{}".format(self.twitter)
 
 @dataclasses.dataclass
 class Organization(BaseContact):
     """An business or organization"""
     branches: list['Organization'] = None
+
 
 @dataclasses.dataclass
 class Person(BaseContact):
@@ -35,11 +40,12 @@ class Person(BaseContact):
     company: Organization = None
     title: str = None
     job_title: str = None
+    twitter: str = None
 
-    def __str__(self):
+    def __str__(self): # It shouldn't be returning a literal like that
         """Return a multi-line string representation of the person"""
         
-        return """{self.name=}
+        return f"""{self.name=}
 {self.phone=}
 {self.address=}
 {self.notes=}
@@ -47,9 +53,19 @@ class Person(BaseContact):
 """
 
 @dataclasses.dataclass
+class Team:
+    """An individual team"""
+    name: str
+    personnel: list['Team', Person] = dataclasses.field(default_factory=list)
+
+    def __str__(self):
+        """Return the team name"""
+        return f"<Team {self.name}>"
+
+@dataclasses.dataclass
 class AddressBook:
     owner: Person = None
-    contacts: list[Person, Organization] = dataclasses.field(default_factory=list)
+    contacts: list[Person, Organization, Team] = dataclasses.field(default_factory=list)
 
     def __str__(self):
         return f"Address Book for {self.owner.name}"
