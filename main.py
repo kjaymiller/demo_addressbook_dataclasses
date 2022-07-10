341cce50f3a9531da1e31040c117e8551470f2d0
Jay Miller <kjaymiller@gmail.com>
2022-07-09 23:54:07 -0700

    add team data

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import addressbook
+import addressbook 
 
 jay = addressbook.Person(name="Jay", twitter="@kjaymiller")
 nina = addressbook.Person(name="Nina", twitter="@nnja")
@@ -6,15 +6,19 @@ anthony = addressbook.Person(name="Anthony", twitter="@anthonypjshaw")
 sarah = addressbook.Person(name="Sarah", twitter="@crazy4pi314")
 pamela = addressbook.Person(name="Pamela", twitter="@pamelafox")
 
+
 MS_Python = addressbook.Team(
     name = "Team Python",
     members = [jay, sarah, nina, anthony, pamela]
 )
 
-def get_twitter_accounts_to_follow(team: addressbook.Team):
+def get_twitter_accounts_to_follow(team: addressbook.Team) -> str:
     """return a list of all the team members twitter accounts"""
     return "\n".join([member.get_twitter_url() for member in team.members])
 
-
 if __name__ == "__main__":
     print(get_twitter_accounts_to_follow(MS_Python))
+
+
+
+
